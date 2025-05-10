0c76aaddf8732d432c22bc8914d0a98a0d942faf
mellow65 <126365319+mellow65@users.noreply.github.com>
2025-05-10 15:26:40 -0700

    Update newt_installer.sh

--- newt_installer.sh
+++ newt_installer.sh
@@ -79,8 +79,8 @@ if [ -f "$SERVICE_FILE" ]; then
             read -p "Enter your Newt ID: " ID
             read -p "Enter your Newt Secret: " SECRET
 
-            echo "⚠️ Please go back to the Pangolin site and click 'I have copied the config' then 'Create Site'."
-            echo "Once you have completed those steps, return here and press Enter to continue. ⚠️"
+            echo "⚠ Please go back to the Pangolin site and click 'I have copied the config' then 'Create Site'."
+            echo "Once you have completed those steps, return here and press Enter to continue. ⚠"
             read -p "Press Enter to continue..."
 
             echo "🔄 Stopping Newt service..."
